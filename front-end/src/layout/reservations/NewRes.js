14db2558ec034a5421aa26e6a89206fede082b84
Kate <neilsenk09@gmail.com>
2022-07-19 01:03:43 -0400

    user story 1

--- front-end/src/layout/reservations/NewRes.js
+++ front-end/src/layout/reservations/NewRes.js
@@ -1,74 +1,119 @@
 import React, { useState } from "react";
-import "./newRes.css";
+import "./reservations.css";
 import { useHistory } from "react-router-dom";
+import { createReservation } from "../../utils/api";
 
-export default function NewRes() {
-  const [reservation, setReservation] = useState({
+export default function NewRes({ reservations }) {
+  const initialState = {
     first_name: "",
     last_name: "",
     mobile_number: "",
     reservation_date: "",
     reservation_time: "",
     people: "",
-  });
+  };
+
+  const [reservation, setReservation] = useState({ ...initialState });
 
   const history = useHistory();
 
-  function submitHandler(event) {
+  async function submitHandler(event) {
     event.preventDefault();
-    history.push("/");
+    // console.log(reservation);
+    await createReservation(reservation);
+    validate(formValues);
+    history.push("/dashboard");
+  }
+
+  function changeHandler(event) {
+    setReservation({ ...reservation, [event.target.name]: event.target.value });
+    // console.log(reservation);
   }
 
   function cancelHandler() {
-    history.back();
+    history.goBack();
   }
-
   return (
     <div className="container">
-      <div className="title">
-        <h1>New Reservation</h1>
-      </div>
-      <form>
-        <label>
-          First name:
-          <input name="first_name" type="text" />
-        </label>
-        <br />
-        <label>
-          Last name:
-          <input name="last_name" type="text" />
-        </label>
-        <br />
-        <label>
-          Mobile number:
-          <input name="mobile_number" type="text" />
-        </label>
-        <br />
-        <label>
-          Date of reservation:
-          <input name="reservation_date" type="date" />
-        </label>
-        <br />
-        <label>
-          Time of reservation:
-          <input name="reservation_time" type="time" />
-        </label>
-        <br />
-        <label>
-          {/* Number of people must be at least 1 */}
-          Number of people:
-          <input name="people" type="number" />
-        </label>
-        <br />
-        <div>
-          <button type="submit" onClick={submitHandler}>
-            Submit
-          </button>
-          <button type="submit" onClick={cancelHandler}>
-            Cancel
-          </button>
+      <div className="row">
+        <div className="title">
+          <h1>New Reservation</h1>
         </div>
-      </form>
+        <form onSubmit={submitHandler}>
+          <label>
+            First name:
+            <input
+              className="form-control"
+              name="first_name"
+              type="text"
+              value={reservation.first_name}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            Last name:
+            <input
+              className="form-control"
+              name="last_name"
+              type="text"
+              value={reservation.last_name}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            Mobile number:
+            <input
+              className="form-control"
+              name="mobile_number"
+              type="text"
+              value={reservation.mobile_number}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            Date of reservation:
+            <input
+              className="form-control"
+              name="reservation_date"
+              type="date"
+              value={reservation.reservation_date}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            Time of reservation:
+            <input
+              className="form-control"
+              name="reservation_time"
+              type="time"
+              value={reservation.reservation_time}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            Number of people:
+            <input
+              className="form-control"
+              name="people"
+              type="number"
+              value={reservation.people}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <div>
+            <button type="submit">Submit</button>
+            <button type="submit" onClick={cancelHandler}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
