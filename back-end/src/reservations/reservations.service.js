e0bee8f53df8f0ecc081c2472e0a60d92710acdd
Kate <neilsenk09@gmail.com>
2022-07-20 14:59:20 -0400

    methodNotAllowed middleware

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,9 @@
 const knex = require("../db/connection");
 
-function list(reservation_date) {
+function list(date) {
   return knex("reservations")
     .select("*")
-    .where({ reservation_date })
+    .where({ reservation_date: date })
     .orderBy("reservation_time");
 }
 
